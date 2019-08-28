47843ff61cc87c705a2eef38ffc447ccbffabd7c
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2019-08-29 00:45:28 +0300

    Отображаются номера пар, пары без указанных преподов не отображают серый чип

--- feed/src/main/java/kekmech/ru/feed/items/CoupleItem.kt
+++ feed/src/main/java/kekmech/ru/feed/items/CoupleItem.kt
@@ -1,10 +1,12 @@
 package kekmech.ru.feed.items
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.view.View
 import android.widget.TextView
 import kekmech.ru.core.dto.CoupleNative
 import kekmech.ru.coreui.Chip
+import kekmech.ru.coreui.Resources
 import kekmech.ru.coreui.adapter.BaseFactory
 import kekmech.ru.coreui.adapter.BaseItem
 import kekmech.ru.coreui.adapter.BaseViewHolder
@@ -14,20 +16,18 @@ class CoupleItem(val couple: CoupleNative) : BaseItem<CoupleItem.ViewHolder>() {
 
     @SuppressLint("DefaultLocale")
     override fun updateViewHolder(viewHolder: ViewHolder) {
-        viewHolder.name.text = couple.name ?: ""
+        viewHolder.name.text = couple.name
         viewHolder.typeLecture.visibility = if (couple.type == CoupleNative.LECTURE) View.VISIBLE  else View.GONE
         viewHolder.typePractice.visibility = if (couple.type == CoupleNative.PRACTICE) View.VISIBLE  else View.GONE
         viewHolder.typeLab.visibility = if (couple.type == CoupleNative.LAB) View.VISIBLE else View.GONE
-        viewHolder.overline.text = "Очередная пара".toUpperCase() //couple.num
-        viewHolder.place.text = couple.place ?: ""
-        viewHolder.timeStart.text = couple.timeStart ?: ""
-        viewHolder.timeEnd.text = couple.timeEnd ?: ""
-        if (couple.teacher != null) {
+        viewHolder.overline.text = getCoupleNumAsString(viewHolder.itemView.context).toUpperCase()
+        viewHolder.place.text = couple.place
+        viewHolder.timeStart.text = couple.timeStart
+        viewHolder.timeEnd.text = couple.timeEnd
+        if (couple.teacher.isNotEmpty()) {
             viewHolder.teacher.visibility = View.VISIBLE
-            viewHolder.teacher.text = couple.teacher!!
-        } else {
-            viewHolder.teacher.visibility = View.GONE
-        }
+            viewHolder.teacher.text = couple.teacher
+        } else viewHolder.teacher.visibility = View.GONE
     }
 
     override fun approveFactory(factory: BaseFactory) = factory is Factory
@@ -47,5 +47,21 @@ class CoupleItem(val couple: CoupleNative) : BaseItem<CoupleItem.ViewHolder>() {
 
     class Factory: BaseFactory(R.layout.item_couple_full_layout) {
         override fun instance(view: View) = ViewHolder(view)
+    }
+
+
+    private fun getCoupleNumAsString(context: Context): String {
+        if (couple.num in 1..5) {
+            if (coupleNames == null) coupleNames = Resources.getStringArray(context, R.array.couples_numbers)
+            return coupleNames!![couple.num - 1]
+        } else {
+            if (coupleUnknown == null) coupleUnknown = Resources.getString(context, R.string.couple_without_number)
+            return coupleUnknown!!
+        }
+    }
+
+    companion object {
+        private var coupleNames: Array<String>? = null
+        private var coupleUnknown: String? = null
     }
 }
