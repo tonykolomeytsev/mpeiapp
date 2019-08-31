4656e245ad944963a24b39f6bbfd2699756034ca
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2019-09-01 00:02:57 +0300

    В утилиту времени Time добавил возможность генерации предыдущих и следующих дней по offset

--- repository/src/main/java/kekmech/ru/repository/utils/Time.kt
+++ repository/src/main/java/kekmech/ru/repository/utils/Time.kt
@@ -27,6 +27,14 @@ class Time(private val calendar: Calendar = Calendar.getInstance()) {
         return@lazy 1 + weekOfYear - firstDay.weekOfYear
     }
     val parity by lazy { if (weekOfSemester % 2 == 0) Parity.EVEN else Parity.ODD }
+    val nextDay by lazy { Time(Calendar.getInstance().apply { timeInMillis = calendar.timeInMillis + MILLIS_IN_DAY } ) }
+    val prevDay by lazy { Time(Calendar.getInstance().apply { timeInMillis = calendar.timeInMillis - MILLIS_IN_DAY } ) }
+
+    /**
+     * Получить день, который будет через [dayOffset] дней
+     */
+    fun getDayWithOffset(dayOffset: Int) =
+        Time(Calendar.getInstance().apply { timeInMillis = calendar.timeInMillis + (MILLIS_IN_DAY * dayOffset) } )
 
     /**
      * Форматирование времени к виду "Часы:Минуты"
@@ -38,9 +46,18 @@ class Time(private val calendar: Calendar = Calendar.getInstance()) {
      * @param context - контекст через который можно получить доступ к strings.xml
      * @param stringArrayId - массив с названиями дней недели (ВСК, ПН, ВТ, СР ...)
      * @return [String] - соответствующее номеру названия дня недели
-     * *Обрати внимание, что воскресенье считается первым днем недели (id = 1)
+     * *Calendar.SUNDAY == 1 (недели начинаются с воскресенья, нумерация дней с единицы)
      */
     fun formattedAsDayName(context: Context?, stringArrayId: Int) = getStringArray(context, stringArrayId)[dayOfWeek - 1]
+
+    /**
+     * Форматирование к виду "Месяц"
+     * @param context - контекст через который можно получить доступ к strings.xml
+     * @param stringArrayId - массив с названиями дней недели (ЯНВАРЬ, ФЕВРАЛЬ, МАРТ, ...)
+     * @return [String] - соответствующее номеру названия дня недели
+     * *Calendar.JANUARY == 0 (нумерация месяцев с нуля)
+     */
+    fun formattedAsMonthName(context: Context?, stringArrayId: Int) = getStringArray(context, stringArrayId)[month]
 
     private fun getStringArray(context: Context?, stringArrayId: Int): Array<String> {
         if (context == null) {
@@ -55,6 +72,11 @@ class Time(private val calendar: Calendar = Calendar.getInstance()) {
     enum class SemesterType { FALL, SPRING }
 
     companion object {
+        const val HOURS_IN_DAY = 24
+        const val MINUTES_IN_DAY = HOURS_IN_DAY * 60
+        const val SECONDS_IN_DAY = MINUTES_IN_DAY * 60
+        const val MILLIS_IN_DAY = SECONDS_IN_DAY * 1000
+
         fun today() = Time(Calendar.getInstance())
 
         /**
