a7f871dca5b64790a066e435b9e77cad95f6092f
tonykolomeytsev <tonykolomeytsev@gmail.com>
2019-09-21 12:37:22 +0300

    Code cleanup in HTMLWorker - optimize imports - remove unused code - migrate from deprecated StringEscapeUtils lib to new version - rename tryGroup to tryGroupAsync

--- addscreen/src/main/java/kekmech/ru/addscreen/presenter/HTMLWorker.kt
+++ addscreen/src/main/java/kekmech/ru/addscreen/presenter/HTMLWorker.kt
@@ -1,10 +1,6 @@
 package kekmech.ru.addscreen.presenter
 
-import android.annotation.SuppressLint
-import android.graphics.Bitmap
-import android.webkit.WebResourceRequest
 import android.webkit.WebView
-import android.webkit.WebViewClient
 import kekmech.ru.addscreen.parser.HtmlToScheduleParser
 import kekmech.ru.addscreen.parser.ParserCouple
 import kekmech.ru.addscreen.parser.ParserSchedule
@@ -13,13 +9,9 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.withContext
-import org.apache.commons.lang3.StringEscapeUtils
+import org.apache.commons.text.StringEscapeUtils
 
 class HTMLWorker(private val webView: WebView) {
-
-    private lateinit var group: String
-    private lateinit var parser: HtmlToScheduleParser
-    private lateinit var web: Wrapper
 
     private fun formSubmitScript(group: String) =
         "document.getElementsByName('$TEXTBOX_NAME')[0].value = '$group';" +
@@ -30,7 +22,7 @@ class HTMLWorker(private val webView: WebView) {
     private fun timetableUrl(groupId: String, startDate: Time) =
         "$MPEI_TIMETABLE?groupoid=$groupId&start=${startDate.formattedAsYearMonthDay}"
 
-    suspend fun tryGroup(group: String) = GlobalScope.async(Dispatchers.IO) {
+    suspend fun tryGroupAsync(group: String) = GlobalScope.async(Dispatchers.IO) {
         // загружаем страничку и вбиваем номер группы в форму
         WebkitLatch(webView).async { loadUrl(MPEI_DEFAULT) }
         val groupId = WebkitLatch(webView).async {
@@ -61,7 +53,7 @@ class HTMLWorker(private val webView: WebView) {
             // объединяем результаты парсинга
             val joinedCouples = mutableListOf<ParserCouple>()
             joinedCouples.addAll(it.first.couples)
-            joinedCouples.addAll(it.second.couples.onEach { it.week = 2 })
+            joinedCouples.addAll(it.second.couples.onEach { couple -> couple.week = 2 })
             ParserSchedule(
                 joinedCouples,
                 firstMonday.calendar
@@ -79,82 +71,10 @@ class HTMLWorker(private val webView: WebView) {
         .first { it.value.matches("\\d+".toRegex()) }
         .value
 
-    @SuppressLint("SetJavaScriptEnabled")
-    class Wrapper(private val webView: WebView) {
-        @Volatile
-        var locker = false
-        @Volatile
-        var url = ""
-        @Volatile
-        var urls = mutableListOf<String>()
-
-        fun lock() {
-            locker = true
-        }
-
-        fun unlock() {
-            locker = false
-        }
-
-        init {
-            webView.post {
-                webView.settings.apply {
-                    javaScriptEnabled = true
-                    setSupportMultipleWindows(false)
-                    loadsImagesAutomatically = false
-                    defaultTextEncodingName = "utf-8"
-                }
-                webView.webViewClient = object : WebViewClient() {
-                    var weekNum = 1
-
-                    override fun shouldOverrideUrlLoading(
-                        view: WebView?,
-                        request: WebResourceRequest?
-                    ): Boolean {
-                        view?.loadUrl(request?.url.toString())
-                        return false
-                    }
-
-                    override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
-                        lock()
-                    }
-
-                    override fun onPageFinished(view: WebView?, url: String?) {
-                        unlock()
-                        println("onPageFinished " + url!!)
-                        urls.add(url)
-                        this@Wrapper.url = url.toString()
-                    }
-                }
-            }
-        }
-
-        fun post(action: () -> Unit) = webView.post(action)
-
-        fun js(script: String, lock: Boolean = false, callback: ((String) -> Unit)?) {
-            if (lock) locker = true
-            post { webView.evaluateJavascript(script, callback) }
-        }
-
-        suspend fun awaitSync() {
-            while (GlobalScope.async(Dispatchers.IO) { locker }.await());
-        }
-
-        suspend fun load(url: String) {
-            this.url = ""
-            locker = true
-            post { webView.loadUrl(url) }
-            awaitSync()
-        }
-    }
-
     companion object {
         const val MPEI_DEFAULT = "https://mpei.ru/Education/timetable/Pages/default.aspx"
         const val MPEI_TIMETABLE = "https://mpei.ru/Education/timetable/Pages/table.aspx"
         const val TEXTBOX_NAME = "ctl00\$ctl30\$g_f0649160_e72e_4671_a36b_743021868df5\$ctl03"
-        const val BUTTON_NAME = "ctl00\$ctl30\$g_f0649160_e72e_4671_a36b_743021868df5\$ctl04"
-
-        const val LOADING_DEFAULT = 0
-        const val FILLING = 1
+        const val BUTTON_NAME =  "ctl00\$ctl30\$g_f0649160_e72e_4671_a36b_743021868df5\$ctl04"
     }
 }
