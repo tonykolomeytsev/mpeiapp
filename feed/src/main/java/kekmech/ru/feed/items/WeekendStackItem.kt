b49f63879ed98eaa86d0a5a522adcb2d86ed38c8
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2019-09-24 01:05:45 +0300

    Added emoji reaction on weekendStackItem

--- feed/src/main/java/kekmech/ru/feed/items/WeekendStackItem.kt
+++ feed/src/main/java/kekmech/ru/feed/items/WeekendStackItem.kt
@@ -1,6 +1,7 @@
 package kekmech.ru.feed.items
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import kekmech.ru.coreui.R
 import kekmech.ru.coreui.adapter.BaseFactory
@@ -17,6 +18,17 @@ class WeekendStackItem(
         viewHolder.header.text = headerUpper
         viewHolder.footer.text = headerBottom
         viewHolder.spacing.visibility = if (isLiveVisible) View.VISIBLE else View.GONE
+        listOf(
+            R.drawable.emoji_smile,
+            R.drawable.emoji_love,
+            R.drawable.emoji_hooray,
+            R.drawable.emoji_stars,
+            R.drawable.emoji_deal_with,
+            R.drawable.emoji_cucumber
+        )
+            .shuffled()
+            .take(3)
+            .forEachIndexed { i, e -> viewHolder.emojis[i].setImageResource(e) }
     }
 
     override fun approveFactory(factory: BaseFactory) = factory is Factory
@@ -25,6 +37,10 @@ class WeekendStackItem(
         val spacing by bind<View>(R.id.weekendItemSpacing)
         val header by bind<TextView>(R.id.textViewWeekendHeader)
         val footer by bind<TextView>(R.id.textViewWeekendFooter)
+        val emoji1 by bind<ImageView>(R.id.emoji1)
+        val emoji2 by bind<ImageView>(R.id.emoji2)
+        val emoji3 by bind<ImageView>(R.id.emoji3)
+        val emojis by lazy { arrayOf(emoji1, emoji2, emoji3) }
         override fun onCreateView(view: View) = Unit
     }
 
