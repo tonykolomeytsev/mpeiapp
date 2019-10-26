4ccfb40c5e547a02a1bcbc7b1559528170c4481d
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2019-10-26 18:31:21 +0300

    Improve BaseFactory class

--- coreui/src/main/java/kekmech/ru/coreui/adapter/BaseFactory.kt
+++ coreui/src/main/java/kekmech/ru/coreui/adapter/BaseFactory.kt
@@ -8,11 +8,11 @@ import android.view.ViewGroup
  * Created by Kolomeytsev Anton on 09.07.2016.
  * This class is a part of Mr. Captain project.
  */
-abstract class BaseFactory(val layoutId: Int, val viewHolderGenerator: () -> BaseViewHolder) {
+abstract class BaseFactory(val layoutId: Int, val viewHolderGenerator: (View) -> BaseViewHolder) {
 
     fun instanceNative(parent: ViewGroup?, inflater: LayoutInflater): BaseViewHolder {
         return instance(inflater.inflate(layoutId, parent, false))
     }
 
-    open fun instance(view: View): BaseViewHolder = viewHolderGenerator()
+    open fun instance(view: View): BaseViewHolder = viewHolderGenerator(view)
 }
