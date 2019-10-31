f7d7cd6ced8e0711ab0c3598e8812ef7bdfad8a6
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2019-10-31 21:24:40 +0300

    Добавил запросы для загрузки странички с оценками

--- repository/src/main/java/kekmech/ru/repository/BarsRepositoryImpl.kt
+++ repository/src/main/java/kekmech/ru/repository/BarsRepositoryImpl.kt
@@ -1,6 +1,31 @@
 package kekmech.ru.repository
 
 import kekmech.ru.core.repositories.BarsRepository
+import org.jsoup.Jsoup
 import javax.inject.Inject
 
-class BarsRepositoryImpl @Inject constructor() : BarsRepository
+class BarsRepositoryImpl @Inject constructor() : BarsRepository {
+
+
+    private fun loadFromBars() {
+        val mainPage = Jsoup.connect("https://bars.mpei.ru/bars_web/")
+            .get()
+        val stoken = mainPage
+            .select("input[name=SToken]")
+            .`val`()
+        val requestVirificationToken = mainPage
+            .select("input[name=__RequestVerificationToken]")
+            .`val`()
+        println(stoken)
+        println(requestVirificationToken)
+
+        val response = Jsoup.connect("https://bars.mpei.ru/bars_web/")
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .data("Password", "")
+            .data("Username", "")
+            .data("Remember", "false")
+            .data("SToken", stoken)
+            .data("__RequestVerificationToken", requestVirificationToken)
+            .post()
+    }
+}
