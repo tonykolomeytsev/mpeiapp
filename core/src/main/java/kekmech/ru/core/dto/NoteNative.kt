b63703f522afca661451618b1a5631f9a5d8efae
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2019-11-02 02:44:49 +0300

    Мигрировал на версию БД №2, (с таблицей notes) #24

---
+++ core/src/main/java/kekmech/ru/core/dto/NoteNative.kt
@@ -0,0 +1,33 @@
+package kekmech.ru.core.dto
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import org.jetbrains.annotations.NotNull
+
+@Entity(
+    tableName = "notes",
+    indices = [Index(
+        value = ["id"],
+        unique = true
+    )]
+)
+data class NoteNative(
+    @ColumnInfo(name = "couple_id")
+    var coupleId: Int,
+
+    @ColumnInfo(name = "timestamp")
+    var timestamp: String,
+
+    @ColumnInfo(name = "data")
+    var data: String,
+
+    @ColumnInfo(name = "tag")
+    var tag: String
+) {
+
+    @PrimaryKey(autoGenerate = true)
+    @NotNull
+    var id: Int = 0
+}
