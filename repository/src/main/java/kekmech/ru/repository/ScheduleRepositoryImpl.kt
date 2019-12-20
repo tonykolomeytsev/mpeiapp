00c627cd29dfaa06cf1a3fea3e312a0bc70fa8bf
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2019-12-20 03:08:56 +0300

    Добавлен скраппер для расписания сессии, добирается до HTML таблицы с расписанием, но пока не умеет парсить

--- repository/src/main/java/kekmech/ru/repository/ScheduleRepositoryImpl.kt
+++ repository/src/main/java/kekmech/ru/repository/ScheduleRepositoryImpl.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import org.jsoup.Jsoup
 import java.util.*
 import javax.inject.Inject
 
@@ -62,5 +63,47 @@ class ScheduleRepositoryImpl @Inject constructor(
     override fun getAllSchedules() = scheduleCacheGateway.getAllSchedules()
 
     override fun setCurrentScheduleId(id: Int) = scheduleCacheGateway.setCurrentScheduleId(id)
+
+    fun loadSessionFromRemote() {
+        val inputs = Jsoup.connect("https://mpei.ru/Education/timetable/Pages/default.aspx")
+            .get()
+            .select("input")
+        val eventValidationInput = inputs.find { it.attr("name") == "__EVENTVALIDATION" }!!
+        val viewStateInput = inputs.find { it.attr("name") == "__VIEWSTATE" }!!
+        val groupNameInput = inputs.find { it.attr("name").matches("ctl00\\\$ctl30.*ctl03".toRegex()) }!!
+        val groupSubmitInput = inputs.find { it.attr("name").matches("ctl00\\\$ctl30.*ctl04".toRegex()) }!!
+
+
+        val href = Jsoup.connect("https://mpei.ru/Education/timetable/Pages/default.aspx")
+            .data(eventValidationInput.attr("name"), eventValidationInput.attr("value"))
+            .data(viewStateInput.attr("name"), viewStateInput.attr("value"))
+            .data(groupNameInput.attr("name"), "С-12-16")
+            .data(groupSubmitInput.attr("name"), groupSubmitInput.attr("value"))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .followRedirects(false)
+            .post()
+            .select("a[href]")
+            .first()
+            .attr("href")
+
+        val timetable = Jsoup.connect(href)
+            .post()
+        val __EVENTVALIDATION = timetable.select("input[name=__EVENTVALIDATION]").attr("value")
+        val __VIEWSTATE = timetable.select("input[name=__VIEWSTATE]").attr("value")
+        val __EVENTARGUMENT = "0"
+        val __VIEWSTATEGENERATOR = timetable.select("input[name=__VIEWSTATEGENERATOR]").attr("value")
+        val __EVENTTARGET = timetable.select("div[class=mpei-tt-outer-wrap]").select("a").attr("href").let {
+            ".*'(.*)'.*'.*'.*".toRegex().find(it)?.groups?.get(1)?.value ?: ""
+        }
+        val result = Jsoup.connect(href)
+            .data("__EVENTVALIDATION", __EVENTVALIDATION)
+            .data("__VIEWSTATE", __VIEWSTATE)
+            .data("__EVENTARGUMENT", __EVENTARGUMENT)
+            .data("__VIEWSTATEGENERATOR", __VIEWSTATEGENERATOR)
+            .data("__EVENTTARGET", __EVENTTARGET)
+            .followRedirects(true)
+            .post()
+        println(result.html())
+    }
 
 }
