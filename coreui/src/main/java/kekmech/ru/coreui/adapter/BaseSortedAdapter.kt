aa7f6ff145e8dd1891e0fc4ebc9881503d1bdd64
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2020-01-03 16:23:26 +0300

    Оптимизированна работа адаптера

--- coreui/src/main/java/kekmech/ru/coreui/adapter/BaseSortedAdapter.kt
+++ coreui/src/main/java/kekmech/ru/coreui/adapter/BaseSortedAdapter.kt
@@ -5,12 +5,26 @@ import kotlin.reflect.KClass
 open class BaseSortedAdapter : BaseAdapter() {
 
     private var order: Map<Class<out BaseItem<*>>, Int> = emptyMap()
+    private var isAllowedOnlyUniqueItems: Boolean = false
 
     override fun addItem(baseItem: BaseItem<*>) {
         fun index(item: BaseItem<*>) = order[item::class.java]
-        items += baseItem
-        items.sortBy(::index)
-        notifyItemInserted(items.indexOf(baseItem))
+        if (!isAllowedOnlyUniqueItems) {
+            items += baseItem
+            items.sortBy(::index)
+            notifyItemInserted(items.indexOf(baseItem))
+        } else {
+            val similarItem = items.firstOrNull { it.javaClass == baseItem.javaClass }
+            if (similarItem != null) {
+                val index = items.indexOf(similarItem)
+                items[index] = baseItem
+                notifyItemChanged(index)
+            } else {
+                items += baseItem
+                items.sortBy(::index)
+                notifyItemInserted(items.indexOf(baseItem))
+            }
+        }
     }
 
     class Builder {
@@ -23,6 +37,11 @@ open class BaseSortedAdapter : BaseAdapter() {
 
         fun addItemsOrder(list: List<KClass<out BaseItem<*>>>): Builder {
             adapter.order = list.mapIndexed { i, e -> e.java to i }.toMap()
+            return this
+        }
+
+        fun allowOnlyUniqueItems(): Builder {
+            adapter.isAllowedOnlyUniqueItems = true
             return this
         }
 
