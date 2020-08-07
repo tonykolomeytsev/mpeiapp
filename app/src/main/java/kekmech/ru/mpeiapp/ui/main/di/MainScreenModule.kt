d67bc62918baacb32431aba11574ba13d5d3c1bb
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2020-08-08 01:27:18 +0300

    Goto MainFragment

--- app/src/main/java/kekmech/ru/mpeiapp/ui/main/di/MainScreenModule.kt
+++ app/src/main/java/kekmech/ru/mpeiapp/ui/main/di/MainScreenModule.kt
@@ -1,10 +1,17 @@
 package kekmech.ru.mpeiapp.ui.main.di
 
 import kekmech.ru.common_di.ModuleProvider
+import kekmech.ru.common_navigation.di.MainFragmentHolder
 import kekmech.ru.mpeiapp.ui.main.BottomTabsSwitcher
 import kekmech.ru.mpeiapp.ui.main.BottomTabsSwitcherImpl
+import kekmech.ru.mpeiapp.ui.main.MainFragment
 import org.koin.dsl.bind
 
 object MainScreenModule : ModuleProvider({
     single { BottomTabsSwitcherImpl } bind BottomTabsSwitcher::class
+
+    val mainFragmentHolder = object : MainFragmentHolder {
+        override fun invoke() = MainFragment.newInstance()
+    }
+    single { mainFragmentHolder } bind MainFragmentHolder::class
 })
---
+++ common_navigation/src/main/java/kekmech/ru/common_navigation/di/MainFragmentHolder.kt
@@ -0,0 +1,5 @@
+package kekmech.ru.common_navigation.di
+
+import androidx.fragment.app.Fragment
+
+interface MainFragmentHolder : () -> Fragment
