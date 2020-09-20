4ed1ab8355e4b4b57b7eeeb1bf52c49a676630e1
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2020-09-20 15:59:04 +0300

    Increased build speed

--- feature_settings/src/main/java/kekmech/ru/settings/view/SettingsFragmentView.kt
+++
@@ -1,9 +0,0 @@
-package kekmech.ru.settings.view
-
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.LifecycleOwner
-
-interface SettingsFragmentView : LifecycleOwner {
-
-    val recreatingActivity: AppCompatActivity
-}
