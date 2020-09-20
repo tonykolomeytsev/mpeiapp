7002cee17f36344d5d343130300ce2f2b0c22bbd
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2020-09-20 15:59:04 +0300

    Increased build speed

--- feature_settings/src/main/java/kekmech/ru/settings/di/SettingsModule.kt
+++
@@ -1,8 +0,0 @@
-package kekmech.ru.settings.di
-
-import kekmech.ru.settings.SettingsPresenter
-import org.koin.dsl.module
-
-val KoinSettingsModule = module {
-    single { SettingsPresenter(get(), get(), get(), get(), get(), get(), get(), get(), get()) }
-}
