14d8ce20e99382fde43a86242054286c1bb2c375
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2020-09-23 20:35:25 +0300

    Optimize build, remove old code

--- feature_map/src/main/java/kekmech/ru/map/MapCustomizer.kt
+++ feature_map/src/main/java/kekmech/ru/map/MapCustomizer.kt
@@ -1,7 +1,7 @@
 package kekmech.ru.map
 
+import android.annotation.SuppressLint
 import android.content.Context
-import android.util.Log
 import com.example.map.R
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -9,11 +9,10 @@ import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.Marker
-import kekmech.ru.core.FreqLimiter
 
 class MapCustomizer(private val context: Context) {
-    private val limiter = FreqLimiter()
 
+    @SuppressLint("MissingPermission")
     fun initMap(map: GoogleMap?) {
         map?.apply {
             setMapStyle(MapStyleOptions.loadRawResourceStyle(context, R.raw.google_map_style))
@@ -38,8 +37,6 @@ class MapCustomizer(private val context: Context) {
     }
 
     fun animateCameraTo(map: GoogleMap?, marker: Marker) {
-        Log.d("MapCustomizer", "animateCameraTo")
-        if (!limiter()) return
         map?.apply {
             val cameraPosition = CameraPosition.Builder()
                 .tilt(30f)
@@ -48,6 +45,5 @@ class MapCustomizer(private val context: Context) {
                 .build()
             animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), 200, null)
         }
-
     }
 }
