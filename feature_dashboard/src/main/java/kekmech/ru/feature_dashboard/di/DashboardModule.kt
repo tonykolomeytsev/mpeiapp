ae608841fd95aaf1b8b964fe774b09734911ab53
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2020-09-28 19:29:44 +0300

    IS-48: Added analytics wrapper, implement DashboardAnalytics, fix gradle scripts

--- feature_dashboard/src/main/java/kekmech/ru/feature_dashboard/di/DashboardModule.kt
+++ feature_dashboard/src/main/java/kekmech/ru/feature_dashboard/di/DashboardModule.kt
@@ -1,12 +1,14 @@
 package kekmech.ru.feature_dashboard.di
 
 import kekmech.ru.common_di.ModuleProvider
+import kekmech.ru.feature_dashboard.DashboardAnalytics
 import kekmech.ru.feature_dashboard.presentation.DashboardActor
 import kekmech.ru.feature_dashboard.presentation.DashboardFeatureFactory
 import org.koin.dsl.bind
 
 object DashboardModule : ModuleProvider({
     single { DashboardDependencies(get(), get(), get()) } bind DashboardDependencies::class
-    single { DashboardFeatureFactory(get()) } bind DashboardFeatureFactory::class
-    single { DashboardActor(get(), get()) } bind DashboardActor::class
+    factory { DashboardFeatureFactory(get()) } bind DashboardFeatureFactory::class
+    factory { DashboardActor(get(), get()) } bind DashboardActor::class
+    factory { DashboardAnalytics(get()) } bind DashboardAnalytics::class
 })
