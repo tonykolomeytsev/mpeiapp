6cf17b94c521f0b4e3e0e5f80ef043065ebf64f7
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2020-10-25 23:34:25 +0300

    IS-0: fix runtime injection allocation error

--- feature_bars/src/main/java/kekmech/ru/bars/main/BarsFragment.kt
+++ feature_bars/src/main/java/kekmech/ru/bars/main/BarsFragment.kt
@@ -5,7 +5,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import kekmech.ru.bars.R
 
-class BarsFragment : Fragment(R.layout.fragment_bars) {
+internal class BarsFragment : Fragment(R.layout.fragment_bars) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
