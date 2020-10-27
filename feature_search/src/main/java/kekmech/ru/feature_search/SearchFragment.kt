9ac17d3c20e66016f1861b91d56822aa30b75173
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2020-10-27 21:58:42 +0300

    IS-0: added search view

--- feature_search/src/main/java/kekmech/ru/feature_search/SearchFragment.kt
+++ feature_search/src/main/java/kekmech/ru/feature_search/SearchFragment.kt
@@ -3,12 +3,15 @@ package kekmech.ru.feature_search
 import android.os.Bundle
 import android.view.View
 import kekmech.ru.common_android.addSystemVerticalPadding
+import kekmech.ru.common_android.close
+import kekmech.ru.common_android.showKeyboard
 import kekmech.ru.common_mvi.ui.BaseFragment
 import kekmech.ru.feature_search.di.SearchDependencies
 import kekmech.ru.feature_search.mvi.SearchEffect
 import kekmech.ru.feature_search.mvi.SearchEvent
 import kekmech.ru.feature_search.mvi.SearchFeature
 import kekmech.ru.feature_search.mvi.SearchState
+import kotlinx.android.synthetic.main.fragment_search.*
 import org.koin.android.ext.android.inject
 
 internal class SearchFragment : BaseFragment<SearchEvent, SearchEffect, SearchState, SearchFeature>() {
@@ -23,5 +26,7 @@ internal class SearchFragment : BaseFragment<SearchEvent, SearchEffect, SearchSt
 
     override fun onViewCreatedInternal(view: View, savedInstanceState: Bundle?) {
         view.addSystemVerticalPadding()
+        navBackButton.setOnClickListener { close() }
+        searchView.showKeyboard()
     }
 }
