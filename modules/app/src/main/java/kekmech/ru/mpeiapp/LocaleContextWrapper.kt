abe304d042c455f534923b9a3068d9f6923fdc54
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2020-12-03 17:27:03 +0300

    IS-0: localisation works with api >= 25

--- modules/app/src/main/java/kekmech/ru/mpeiapp/LocaleContextWrapper.kt
+++ modules/app/src/main/java/kekmech/ru/mpeiapp/LocaleContextWrapper.kt
@@ -2,20 +2,29 @@ package kekmech.ru.mpeiapp
 
 import android.content.Context
 import android.content.res.Configuration
+import android.os.Build
 import java.util.*
 
 class LocaleContextWrapper {
 
     fun wrapContext(context: Context): Context {
-        val savedLocale = createLocaleFromSharedPreferences(context)!!
-           // ?: return context
-        Locale.setDefault(savedLocale)
-        val newConfig = Configuration()
+        val savedLocale = createLocaleFromSharedPreferences(context) ?: return context
+
+        val newConfig = Configuration(context.resources.configuration)
         newConfig.setLocale(savedLocale)
-        return context.createConfigurationContext(newConfig)
+        if (Build.VERSION.SDK_INT < 25) {
+            @Suppress("DEPRECATION")
+            context.resources.updateConfiguration(newConfig, context.resources.displayMetrics)
+            return context
+        } else {
+            return context.createConfigurationContext(newConfig)
+        }
     }
 
     private fun createLocaleFromSharedPreferences(context: Context): Locale? {
-        return Locale("en", "RU")
+        val prefs = context.getSharedPreferences("mpeix", Context.MODE_PRIVATE)
+        val lang = prefs.getString("app_lang", null)?.split("_") ?: return null
+        val (language, country) = lang[0] to lang[1]
+        return Locale(language, country)
     }
 }
