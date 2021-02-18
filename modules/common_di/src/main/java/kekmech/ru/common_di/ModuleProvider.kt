0292d0cb58387a8736a87a9a64977490bb5a50f3
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2021-02-18 18:11:30 +0300

    Added detekt to project and CI

--- modules/common_di/src/main/java/kekmech/ru/common_di/ModuleProvider.kt
+++ modules/common_di/src/main/java/kekmech/ru/common_di/ModuleProvider.kt
@@ -3,7 +3,7 @@ package kekmech.ru.common_di
 import org.koin.dsl.ModuleDeclaration
 import org.koin.dsl.module
 
-abstract class ModuleProvider(private val declaration: ModuleDeclaration) {
+class ModuleProvider(declaration: ModuleDeclaration) {
     val provider = module(
         createdAtStart = false,
         override = false,
