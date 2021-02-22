bfa061675ae25cac1d49602aa70f24e79f55e472
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2021-02-22 12:50:22 +0300

    Open Favorites screen from dashboard without adding settings screen to backstack

--- modules/feature_app_settings/src/main/java/kekmech/ru/feature_app_settings/launcher/AppSettingsFeatureLauncherImpl.kt
+++ modules/feature_app_settings/src/main/java/kekmech/ru/feature_app_settings/launcher/AppSettingsFeatureLauncherImpl.kt
@@ -3,6 +3,7 @@ package kekmech.ru.feature_app_settings.launcher
 import kekmech.ru.common_navigation.AddScreenForward
 import kekmech.ru.common_navigation.Router
 import kekmech.ru.domain_app_settings.AppSettingsFeatureLauncher
+import kekmech.ru.domain_app_settings.AppSettingsFeatureLauncher.SubPage.FAVORITES
 import kekmech.ru.feature_app_settings.screens.favorites.FavoritesFragment
 import kekmech.ru.feature_app_settings.screens.main.AppSettingsFragment
 
@@ -11,11 +12,9 @@ internal class AppSettingsFeatureLauncherImpl(
 ) : AppSettingsFeatureLauncher {
 
     override fun launch(subPage: AppSettingsFeatureLauncher.SubPage?) {
-        router.executeCommand(AddScreenForward { AppSettingsFragment() })
-        if (subPage != null) when (subPage) {
-            AppSettingsFeatureLauncher.SubPage.FAVORITES -> {
-                router.executeCommand(AddScreenForward { FavoritesFragment() })
-            }
+        when (subPage) {
+            FAVORITES -> router.executeCommand(AddScreenForward { FavoritesFragment() })
+            else -> router.executeCommand(AddScreenForward { AppSettingsFragment() })
         }
     }
 }
