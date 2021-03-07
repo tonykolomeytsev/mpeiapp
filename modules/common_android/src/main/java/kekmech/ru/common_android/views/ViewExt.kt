690a666f6ee22dcb207111eb8e67b766f998d0a1
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2021-03-07 21:29:52 +0300

    Added click listener with debounce, apply it to classes item click event, fixed open two NotesFragment (#103)

--- modules/common_android/src/main/java/kekmech/ru/common_android/views/ViewExt.kt
+++ modules/common_android/src/main/java/kekmech/ru/common_android/views/ViewExt.kt
@@ -1,9 +1,13 @@
 package kekmech.ru.common_android.views
 
+import android.annotation.SuppressLint
+import android.os.SystemClock
 import android.view.HapticFeedbackConstants
 import android.view.MotionEvent.ACTION_DOWN
 import android.view.View
 import android.view.ViewGroup
+
+private const val DEFAULT_DEBOUNCE_MILLIS = 300L
 
 fun View.setMargins(
     left: Int? = null,
@@ -19,6 +23,7 @@ fun View.setMargins(
     }
 }
 
+@SuppressLint("ClickableViewAccessibility")
 fun View.enableHapticFeedback() {
     isHapticFeedbackEnabled = true
     setOnTouchListener { _, event ->
@@ -27,4 +32,21 @@ fun View.enableHapticFeedback() {
         }
         false
     }
+}
+
+fun View.setOnClickListenerWithDebounce(onClickListener: View.OnClickListener) =
+    setOnClickListenerWithDebounce(DEFAULT_DEBOUNCE_MILLIS, onClickListener)
+
+fun View.setOnClickListenerWithDebounce(debounceDuration: Long, onClickListener: View.OnClickListener) {
+    setOnClickListener(object : View.OnClickListener {
+        private var lastClickMillis = SystemClock.uptimeMillis()
+
+        override fun onClick(v: View?) {
+            val currentClickMillis = SystemClock.uptimeMillis()
+            if (currentClickMillis - lastClickMillis >= debounceDuration) {
+                lastClickMillis = currentClickMillis
+                onClickListener.onClick(v)
+            }
+        }
+    })
 }
