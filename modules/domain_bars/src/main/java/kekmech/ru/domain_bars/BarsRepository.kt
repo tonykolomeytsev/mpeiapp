30480c81abfa7d4669da25c36a3663e5f18865bd
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2021-03-25 17:15:32 +0300

    loadStubJs from resources

--- modules/domain_bars/src/main/java/kekmech/ru/domain_bars/BarsRepository.kt
+++ modules/domain_bars/src/main/java/kekmech/ru/domain_bars/BarsRepository.kt
@@ -1,19 +1,24 @@
 package kekmech.ru.domain_bars
 
+import android.content.res.Resources
 import io.reactivex.Single
 import kekmech.ru.domain_bars.dto.JsKit
 import kekmech.ru.domain_bars.dto.RemoteBarsConfig
+import org.intellij.lang.annotations.Language
 
 class BarsRepository(
-    private val barsService: BarsService
+    private val barsService: BarsService,
+    resources: Resources
 ) {
+    @Language("")
+    private val extractJs = resources.openRawResource(R.raw.extract).bufferedReader().readText()
 
     fun getRemoteBarsConfig(): Single<RemoteBarsConfig> = Single.just(
         RemoteBarsConfig(
             loginLink = "https://bars.mpei.ru/bars_web",
             studentListLink = "https://bars.mpei.ru/bars_web/Student/ListStudent",
             js = JsKit(
-                extractMarks = "",
+                extractData = extractJs,
                 changeSemester = ""
             )
         )
