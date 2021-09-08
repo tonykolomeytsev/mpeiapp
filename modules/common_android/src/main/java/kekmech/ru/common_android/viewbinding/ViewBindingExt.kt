f0459ed9d11d0a86f59b51d7249bb9ba2617c4c8
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2021-09-08 15:23:07 +0300

    Fix viewBinding delegate (#138)

--- modules/common_android/src/main/java/kekmech/ru/common_android/viewbinding/ViewBindingExt.kt
+++ modules/common_android/src/main/java/kekmech/ru/common_android/viewbinding/ViewBindingExt.kt
@@ -2,54 +2,29 @@ package kekmech.ru.common_android.viewbinding
 
 import android.view.View
 import androidx.annotation.IdRes
-import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import org.jetbrains.annotations.ApiStatus
 import kotlin.properties.ReadOnlyProperty
 
+@ApiStatus.Internal
 fun <F : Fragment, VB : ViewBinding> Fragment.viewBinding(
-    viewBinder: (F) -> VB
+    viewBinder: (F) -> VB,
 ): ReadOnlyProperty<F, VB> {
-    return object : ViewBindingProperty<F, VB>(viewBinder) {
-        override fun getLifecycleOwner(thisRef: F) = viewLifecycleOwner
-    }
+    return ViewBindingProperty(viewBinder)
 }
 
 inline fun <F : Fragment, T : ViewBinding> Fragment.viewBinding(
     crossinline vbFactory: (View) -> T,
-    crossinline viewProvider: (F) -> View = Fragment::requireView
+    crossinline viewProvider: (F) -> View = Fragment::requireView,
 ): ReadOnlyProperty<F, T> {
     return viewBinding { fragment: F -> vbFactory(viewProvider(fragment)) }
 }
 
-fun <F : BottomSheetDialogFragment, VB : ViewBinding> BottomSheetDialogFragment.viewBinding(
-    viewBinder: (F) -> VB
-): ReadOnlyProperty<F, VB> {
-    return object : ViewBindingProperty<F, VB>(viewBinder) {
-        override fun getLifecycleOwner(thisRef: F) = viewLifecycleOwner
-    }
-}
-
 inline fun <F : BottomSheetDialogFragment, VB : ViewBinding> BottomSheetDialogFragment.viewBinding(
     crossinline vbFactory: (View) -> VB,
-    crossinline viewProvider: (F) -> View = Fragment::requireView
-): ReadOnlyProperty<F, VB> {
-    return viewBinding { fragment: F -> vbFactory(viewProvider(fragment)) }
-}
-
-fun <F : DialogFragment, VB : ViewBinding> DialogFragment.viewBinding(
-    viewBinder: (F) -> VB
-): ReadOnlyProperty<F, VB> {
-    return object : ViewBindingProperty<F, VB>(viewBinder) {
-        override fun getLifecycleOwner(thisRef: F) = // вот этого момента я пока не понимаю
-            if (thisRef.view == null) thisRef.viewLifecycleOwner else thisRef
-    }
-}
-
-inline fun <F : DialogFragment, VB : ViewBinding> DialogFragment.viewBinding(
-    crossinline vbFactory: (View) -> VB,
-    crossinline viewProvider: (F) -> View = Fragment::requireView
+    crossinline viewProvider: (F) -> View = Fragment::requireView,
 ): ReadOnlyProperty<F, VB> {
     return viewBinding { fragment: F -> vbFactory(viewProvider(fragment)) }
 }
--- modules/common_android/src/main/java/kekmech/ru/common_android/viewbinding/ViewBindingProperty.kt
+++ modules/common_android/src/main/java/kekmech/ru/common_android/viewbinding/ViewBindingProperty.kt
@@ -3,52 +3,56 @@ package kekmech.ru.common_android.viewbinding
 import android.os.Handler
 import android.os.Looper
 import androidx.annotation.MainThread
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.viewbinding.ViewBinding
+import timber.log.Timber
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
-internal abstract class ViewBindingProperty<in T : Any, VB : ViewBinding>(
-    private val viewBinder: (T) -> VB
-) : ReadOnlyProperty<T, VB> {
+internal class ViewBindingProperty<in F : Fragment, VB : ViewBinding>(
+    private val viewBinder: (F) -> VB,
+) : ReadOnlyProperty<F, VB> {
 
     private var viewBinding: VB? = null
-    private val lifecycleObserver = ClearOnDestroyLifecycleObserver()
-    private var thisRef: T? = null
-
-    protected abstract fun getLifecycleOwner(thisRef: T): LifecycleOwner
 
     @MainThread
-    override fun getValue(thisRef: T, property: KProperty<*>): VB {
+    override fun getValue(thisRef: F, property: KProperty<*>): VB {
         viewBinding?.let { return it }
 
-        this.thisRef = thisRef
-        val lifecycle = getLifecycleOwner(thisRef).lifecycle
+        val lifecycle = thisRef.viewLifecycleOwner.lifecycle
+        val viewBinding = viewBinder(thisRef)
+
         if (lifecycle.currentState == Lifecycle.State.DESTROYED) {
-            mainHandler.post { viewBinding = null }
+            Timber.w("Access to viewBinding after Lifecycle is destroyed or hasn't created yet.")
         } else {
-            lifecycle.addObserver(lifecycleObserver)
+            lifecycle.addObserver(ClearOnDestroyLifecycleObserver(this))
+            this.viewBinding = viewBinding
         }
-        return viewBinder(thisRef).also { viewBinding = it }
+        return viewBinding
     }
 
     @MainThread
     fun clear() {
-        val thisRef = thisRef ?: return
-        this.thisRef = null
-        getLifecycleOwner(thisRef).lifecycle.removeObserver(lifecycleObserver)
-        mainHandler.post { viewBinding = null }
+        viewBinding = null
     }
 
-    private inner class ClearOnDestroyLifecycleObserver : DefaultLifecycleObserver {
+    private class ClearOnDestroyLifecycleObserver(
+        private val property: ViewBindingProperty<*, *>,
+    ) : DefaultLifecycleObserver {
 
         @MainThread
-        override fun onDestroy(owner: LifecycleOwner): Unit = clear()
+        override fun onDestroy(owner: LifecycleOwner) {
+            if (!mainHandler.post { property.clear() }) {
+                property.clear()
+            }
+        }
     }
 
     private companion object {
+
         private val mainHandler = Handler(Looper.getMainLooper())
     }
 }
