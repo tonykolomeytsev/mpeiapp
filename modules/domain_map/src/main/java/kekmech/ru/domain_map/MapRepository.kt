d047a20c40cbc4ec7a2e731d54e8e921f5f9a20c
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2021-09-11 15:22:31 +0300

    Temporary disable lifetime cache feature

--- modules/domain_map/src/main/java/kekmech/ru/domain_map/MapRepository.kt
+++ modules/domain_map/src/main/java/kekmech/ru/domain_map/MapRepository.kt
@@ -2,10 +2,7 @@ package kekmech.ru.domain_map
 
 import io.reactivex.rxjava3.core.Single
 import kekmech.ru.common_cache.persistent_cache.PersistentCache
-import kekmech.ru.common_cache.persistent_cache.getOrLoadFrom
-import kekmech.ru.common_cache.persistent_cache.ofComposited
 import kekmech.ru.domain_map.dto.GetMapMarkersResponse
-import java.time.Duration
 
 class MapRepository(
     private val mapService: MapService,
@@ -13,14 +10,16 @@ class MapRepository(
 ) {
 
     private val mapMarkersCache =
-        persistentCache.ofComposited(
+        persistentCache.of(
             key = MAP_CACHE_KEY,
             valueClass = GetMapMarkersResponse::class.java,
-            lifetime = Duration.ofDays(1L),
         )
 
     fun getMarkers(): Single<GetMapMarkersResponse> =
-        mapMarkersCache.getOrLoadFrom(mapService.getMapMarkers())
+        mapService
+            .getMapMarkers()
+            .doOnSuccess(mapMarkersCache::set)
+            .onErrorResumeWith(mapMarkersCache.get().toSingle())
 
     private companion object {
 
