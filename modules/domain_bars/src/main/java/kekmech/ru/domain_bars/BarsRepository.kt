966886b4593544d2576d911e425239c2805c6496
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2021-09-11 12:37:51 +0300

    Implement persistent cache for BARS config

--- modules/domain_bars/src/main/java/kekmech/ru/domain_bars/BarsRepository.kt
+++ modules/domain_bars/src/main/java/kekmech/ru/domain_bars/BarsRepository.kt
@@ -6,6 +6,7 @@ import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.subjects.BehaviorSubject
+import kekmech.ru.common_cache.persistent_cache.PersistentCache
 import kekmech.ru.common_shared_preferences.string
 import kekmech.ru.domain_bars.dto.RawMarksResponse
 import kekmech.ru.domain_bars.dto.RemoteBarsConfig
@@ -16,16 +17,31 @@ class BarsRepository(
     private val barsService: BarsService,
     private val gson: Gson,
     preferences: SharedPreferences,
+    persistentCache: PersistentCache,
 ) {
     private val userBarsCache = BehaviorSubject.createDefault(UserBarsInfo())
+    private val remoteBarsConfigCache = persistentCache
+        .of(
+            key = BARS_CONFIG_CACHE_KEY,
+            valueClass = RemoteBarsConfig::class.java,
+        )
+    private val extractJsCache = persistentCache
+        .of(
+            key = BARS_JS_CACHE_KEY,
+            valueClass = String::class.java,
+        )
     var latestLoadedUrl by preferences.string("bars_last_loaded_link")
 
     fun getRemoteBarsConfig(): Single<RemoteBarsConfig> = barsService
         .getRemoteBarsConfig()
+        .doOnSuccess(remoteBarsConfigCache::set)
+        .onErrorResumeWith(remoteBarsConfigCache.get().toSingle())
 
     fun getExtractJs(): Single<String> = barsService
         .getExtractJs()
         .map { it.charStream().buffered().readText() }
+        .doOnSuccess(extractJsCache::set)
+        .onErrorResumeWith(extractJsCache.get().toSingle())
 
     fun observeUserBars(): Observable<UserBarsInfo> = userBarsCache
         .distinctUntilChanged()
@@ -49,14 +65,18 @@ class BarsRepository(
         copy(group = studentGroup)
     }
 
-    private fun updateUserBarsCache(updater: UserBarsInfo.() -> UserBarsInfo) = userBarsCache
-        .firstOrError()
-        .map { updater(it) }
-        .doOnSuccess { userBarsCache.onNext(updater(it)) }
-        .doOnError { userBarsCache.onError(it) }
-        .ignoreElement()
+    private fun updateUserBarsCache(updater: UserBarsInfo.() -> UserBarsInfo): Completable =
+        userBarsCache
+            .firstOrError()
+            .map { updater(it) }
+            .doOnSuccess { userBarsCache.onNext(updater(it)) }
+            .doOnError { userBarsCache.onError(it) }
+            .ignoreElement()
 
     companion object {
+
         private const val INFO_CHANGING_DEBOUNCE = 150L
+        private const val BARS_CONFIG_CACHE_KEY = "BARS_CONFIG_CACHE_KEY"
+        private const val BARS_JS_CACHE_KEY = "BARS_JS_CACHE_KEY"
     }
 }
