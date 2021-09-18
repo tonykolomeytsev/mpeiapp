f4cd49f94e783f4fc42ae6cdf75f469cfc08a851
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2021-09-18 17:51:48 +0300

    Fix critical bug with map persistent caching

--- modules/domain_map/src/main/java/kekmech/ru/domain_map/MapRepository.kt
+++ modules/domain_map/src/main/java/kekmech/ru/domain_map/MapRepository.kt
@@ -3,6 +3,7 @@ package kekmech.ru.domain_map
 import io.reactivex.rxjava3.core.Single
 import kekmech.ru.common_cache.persistent_cache.PersistentCache
 import kekmech.ru.domain_map.dto.MapMarker
+import kekmech.ru.domain_map.dto.MapMarkersCacheWrapper
 
 class MapRepository(
     private val mapService: MapService,
@@ -12,14 +13,18 @@ class MapRepository(
     private val mapMarkersCache =
         persistentCache.of(
             key = MAP_CACHE_KEY,
-            valueClass = listOf<MapMarker>().javaClass,
+            valueClass = MapMarkersCacheWrapper::class.java,
         )
 
     fun getMarkers(): Single<List<MapMarker>> =
         mapService
             .getMapMarkers()
-            .doOnSuccess(mapMarkersCache::set)
-            .onErrorResumeWith(mapMarkersCache.get().toSingle())
+            .doOnSuccess { mapMarkersCache.set(MapMarkersCacheWrapper(it)) }
+            .onErrorResumeWith(
+                mapMarkersCache
+                    .getOrError()
+                    .map { it.mapMarkers }
+            )
 
     private companion object {
 
---
+++ modules/domain_map/src/main/java/kekmech/ru/domain_map/dto/MapMarkersCacheWrapper.kt
@@ -0,0 +1,3 @@
+package kekmech.ru.domain_map.dto
+
+data class MapMarkersCacheWrapper(val mapMarkers: List<MapMarker>)
