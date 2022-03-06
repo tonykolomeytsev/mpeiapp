3d88536af264332ccae58a505523ab98a0bafbe4
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2022-03-06 11:54:10 +0300

    Fix parsing config
    (cherry picked from commit 1eea29cb70e9caf4ebf5b030d03be4302041bf6e)

--- statics/bars/extract.js
+++ statics/bars/extract.js
@@ -41,7 +41,7 @@ const getRating = (metadataContainer) => {
 };
 const getSemesters = () => {
     const s = Array
-        .from(document.getElementById("ddl_FilterSemester").children)
+        .from(document.querySelector("#bs-select-1 ul").children)
         .map((op) => { return {"id": op.value, "name": op.innerText.trim()} });
     return JSON.stringify({ "semesters": s });
 };
@@ -91,7 +91,7 @@ const extractControlActivityRow = (tr) => {
     }
 }
 const getAllMarks = () => {
-    const container = document.getElementById("div-Student_SemesterSheet");
+    const container = document.getElementById("div-Student_SemesterSheet__Mark");
     const rows = Array.from(container.getElementsByTagName("div"));
     const output = [];
     var i = 0;
@@ -121,11 +121,11 @@ kti.onStudentNameExtracted(studentNameAndGroup[0].trim());
 kti.onStudentGroupExtracted(studentNameAndGroup[1].trim());
 kti.onStudentMetaExtracted(getMeta(metadataContainer));
 
+kti.onMarksExtracted(getAllMarks());
 const semesters = getSemesters();
 const selectedSemesterName = 
     document.getElementsByClassName("filter-option-inner-inner")[0].innerText.trim();
 kti.onSemestersExtracted(semesters, selectedSemesterName);
-kti.onMarksExtracted(getAllMarks());
 
 const rating = getRating(metadataContainer);
 kti.onStudentRatingExtracted(rating);
