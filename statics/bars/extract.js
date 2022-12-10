d2933ba036392325cbdb8c8471d06ee6abe3de45
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2022-12-10 13:46:09 +0300

    Update the extract script in accordance with the new requirements
    (cherry picked from commit 2be5a455ddb8e5cb43ef77adef6cf4a07a040f07)

--- statics/bars/extract.js
+++ statics/bars/extract.js
@@ -1,51 +1,19 @@
-const getMeta = (metadataContainer) => {
+let getMeta = (metadataContainer) => {
     const pair = (arr) => {
-        return {"description": arr[0].trim(), "value": arr[1].trim()};
+        return { "description": arr[0].trim(), "value": arr[1].trim() };
     };
     const m = Array
         .from(metadataContainer.children[2].children)
-        .map((li) => pair(li.innerText.split('-')));
+        .map((li) => pair(li.innerText.split(':')));
     return JSON.stringify({ "meta": m });
 };
-const getRating = (metadataContainer) => {
-    const items = Array
-        .from(document.querySelector("#div-FormHeader ul").children)
-        .map(e => e.children[1]?.textContent || e.children[0]?.textContent)
-        .map(e => parseInt(e.trim()));
-    const complexScore = items[0];
-    const studyScore = items[1];
-    const scienceScore = items[4];
-    const socialScore = items[5];
-    const sportScore = items[7];
-    const socialActivityScore = items[6];
-    
-    const dataRows = metadataContainer.getElementsByTagName("li");
-    const studyYear = Number.parseInt(dataRows.item(0).textContent.substring(6)) || 0;
-    const isBachelor = dataRows.item(1).textContent.includes("бакалавр") || false;
-    const isFirstYearBachelor = studyYear == 1 && isBachelor;
-
-    const ratingWeight = {
-        "studyScoreWeight": (isFirstYearBachelor) ? 0.8 : 0.6,
-        "scienceScoreWeight": (isFirstYearBachelor) ? 0.0 : 0.2,
-        "socialScoreWeight": 0.2
-    }
-    return JSON.stringify({
-        "complexScore": complexScore || 0,
-        "studyScore": studyScore || 0,
-        "scienceScore": scienceScore || 0,
-        "socialScore": socialScore || 0,
-        "sportScore": sportScore || 0,
-        "socialActivityScore": socialActivityScore || 0,
-        "weights": ratingWeight
-    });
-};
-const getSemesters = () => {
+let getSemesters = () => {
     const s = Array
         .from(document.querySelector("#bs-select-1 ul").children)
-        .map((op) => { return {"id": op.value, "name": op.innerText.trim()} });
+        .map((op) => { return { "id": op.value, "name": op.innerText.trim() } });
     return JSON.stringify({ "semesters": s });
 };
-const extractControlActivityRow = (tr) => {
+let extractControlActivityRow = (tr) => {
     const columns = Array
         .from(tr.getElementsByTagName("td"))
         .map((el) => el.innerText.trim());
@@ -56,7 +24,11 @@ const extractControlActivityRow = (tr) => {
     } else if (columns.length == 4) {
         return {
             "type": "CONTROL_ACTIVITY",
-            "name": columns[0],
+            "name": columns[0]
+                .replace(/(\r\n|\n|\r|\t)/gm, "") // remove all newlines and tabs
+                .replace(/\s{2,}/gm, " ") // remove super-spaces
+                .replace(/[(]критерии[)]\s*$/gm, "") // remove dropdown menu
+                .trim(),
             "weight": columns[1] || null,
             "weekNum": columns[2] || null,
             "markAndDate": columns[3] || null
@@ -90,42 +62,48 @@ const extractControlActivityRow = (tr) => {
         return { "type": "UNDEFINED" };
     }
 }
-const getAllMarks = () => {
+let getAllMarks = () => {
     const container = document.getElementById("div-Student_SemesterSheet__Mark");
-    const rows = Array.from(container.getElementsByTagName("div"));
+    const rows = Array.from(container.childNodes).filter((el) => el.tagName == "DIV");
     const output = [];
     var i = 0;
     while (i < rows.length) {
         const e = rows[i];
         if (e.classList.contains("collapse") && i > 0) {
-            const header = rows[i - 1].innerText.split(/[\(\),]/gm);
+            const header = rows[i - 1].innerText.split(/[,]/gm);
             const tableRows = Array
                 .from(rows[i].getElementsByTagName("tr"))
+                .filter((el) => !el.classList.contains("collapse"))
                 .map((el) => extractControlActivityRow(el));
             output.push({
                 "disciplineName": header[0].trim(),
                 "personName": header[1].trim(),
                 "assessmentType": header[2].trim(),
-                "units": header[3].trim(),
+                "units": header[3]?.trim() || "",
                 "activities": tableRows
             });
         }
         i++;
     }
-    return JSON.stringify({"payload": output});
+    return JSON.stringify({ "payload": output });
 };
-metadataContainer = document.getElementById("btnErrorInfo").parentElement;
+let metadataContainer = document.getElementById("btnErrorInfo").parentElement;
 
-const studentNameAndGroup = metadataContainer.children[0].innerText.split(/\(.*\)/gm);
+let studentNameAndGroup = metadataContainer.children[0].innerText.split(/\(.*\)/gm);
 kti.onStudentNameExtracted(studentNameAndGroup[0].trim());
 kti.onStudentGroupExtracted(studentNameAndGroup[1].trim());
-kti.onStudentMetaExtracted(getMeta(metadataContainer));
+try {
+    kti.onStudentMetaExtracted(getMeta(metadataContainer));
+} catch (err) {
+    console.log("Cannot extract metadata from the page")
+}
 
 kti.onMarksExtracted(getAllMarks());
-const semesters = getSemesters();
-const selectedSemesterName = 
+let semesters = getSemesters();
+let selectedSemesterName =
     document.getElementsByClassName("filter-option-inner-inner")[0].innerText.trim();
-kti.onSemestersExtracted(semesters, selectedSemesterName);
-
-const rating = getRating(metadataContainer);
-kti.onStudentRatingExtracted(rating);
+try {
+    kti.onSemestersExtracted(semesters, selectedSemesterName);
+} catch (err) {
+    console.log("Cannot extract semesters from the page")
+}
