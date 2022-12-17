c30ae8163ecb087a6bffb7085668e84542c4c0d2
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2022-12-17 13:04:32 +0300

    Update AGP, Kotlin version, and some dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "7.2.1" apply false
-    id("com.android.library") version "7.2.1" apply false
+    id("com.android.application") version "7.3.1" apply false
+    id("com.android.library") version "7.3.1" apply false
     kotlin("android") version "1.7.0" apply false
     id("com.google.gms.google-services") version "4.3.13" apply false
     id("com.google.firebase.crashlytics") version "2.9.1" apply false
