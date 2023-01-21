146dac4e5178beb416fad9b903f7e4b19c3e403c
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2023-01-21 15:35:48 +0300

    Update JDK version, update detekt version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     kotlin("android") version "1.7.0" apply false
     id("com.google.gms.google-services") version "4.3.13" apply false
     id("com.google.firebase.crashlytics") version "2.9.1" apply false
-    id("io.gitlab.arturbosch.detekt") version "1.20.0"
+    id("io.gitlab.arturbosch.detekt") version "1.22.0"
 
     id("mpeix.android") apply false
     id("mpeix.kotlin") apply false
@@ -22,6 +22,6 @@ subprojects {
         config = rootProject.files("detekt.yaml")
     }
     dependencies {
-        detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.20.0")
+        detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.22.0")
     }
 }
