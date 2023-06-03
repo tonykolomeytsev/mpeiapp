2d8747fb537d1e2523285c788773ee5b48979a7e
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2023-06-03 14:31:04 +0300

    Remove old InMemoryCache (#208)

--- modules/common/cache/src/main/kotlin/kekmech/ru/common_cache/di/CacheModule.kt
+++ modules/common/cache/src/main/kotlin/kekmech/ru/common_cache/di/CacheModule.kt
@@ -1,12 +1,9 @@
 package kekmech.ru.common_cache.di
 
-import kekmech.ru.common_cache.in_memory_cache.InMemoryCache
 import kekmech.ru.common_cache.persistent_cache.PersistentCache
 import kekmech.ru.common_di.AppCacheDir
-import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val CommonCacheModule = module {
-    singleOf(::InMemoryCache)
     single { PersistentCache(get(), get<AppCacheDir>().dir) }
 }
