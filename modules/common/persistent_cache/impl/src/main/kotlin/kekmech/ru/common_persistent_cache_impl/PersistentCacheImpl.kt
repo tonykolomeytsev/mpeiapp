d6990cb6c1c2406227e57af3692ea1e28b757932
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2023-06-04 02:24:22 +0300

    Fix observe() method in persistent cache

--- modules/common/persistent_cache/impl/src/main/kotlin/kekmech/ru/common_persistent_cache_impl/PersistentCacheImpl.kt
+++ modules/common/persistent_cache/impl/src/main/kotlin/kekmech/ru/common_persistent_cache_impl/PersistentCacheImpl.kt
@@ -4,11 +4,13 @@ import kekmech.ru.common_coroutines_api.CoroutineDispatchers
 import kekmech.ru.common_persistent_cache_api.PersistentCache
 import kekmech.ru.common_persistent_cache_api.PersistentCacheKey
 import kekmech.ru.common_persistent_cache_impl.data.PersistentCacheSource
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
@@ -32,6 +34,7 @@ internal class PersistentCacheImpl(
         value: T,
     ): Result<Unit> =
         withContext(dispatchers.io()) {
+            globalSharedFlow.emit(PersistentCache.Entry(key, value))
             runCatching {
                 globalMutex.withLock {
                     source.outputStream(key)
@@ -51,6 +54,7 @@ internal class PersistentCacheImpl(
                             @Suppress("UNCHECKED_CAST")
                             it.readObject() as T
                         }
+                        .also { globalSharedFlow.emit(PersistentCache.Entry(key, it)) }
                 }
             }
         }
@@ -67,8 +71,12 @@ internal class PersistentCacheImpl(
     @Suppress("UNCHECKED_CAST")
     override fun <T : Serializable> observe(
         key: PersistentCacheKey,
-    ): Flow<PersistentCache.Entry<T>> =
-        globalSharedFlow
+    ): Flow<PersistentCache.Entry<T>> {
+        CoroutineScope(dispatchers.io()).launch {
+            restore<T>(key) // ignore result
+        }
+        return globalSharedFlow
             .filter { it.key == key }
             .map { it as PersistentCache.Entry<T> }
+    }
 }
