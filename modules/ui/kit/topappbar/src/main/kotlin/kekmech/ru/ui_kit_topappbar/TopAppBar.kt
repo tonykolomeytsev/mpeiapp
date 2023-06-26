dd98f3792a01205650ef326ef582347573505b64
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2023-06-26 18:06:53 +0300

    Add LargeTopAppBar mapping implementation

--- modules/ui/kit/topappbar/src/main/kotlin/kekmech/ru/ui_kit_topappbar/TopAppBar.kt
+++ modules/ui/kit/topappbar/src/main/kotlin/kekmech/ru/ui_kit_topappbar/TopAppBar.kt
@@ -8,11 +8,13 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kekmech.ru.ui_icons.MpeixIcons
@@ -58,7 +60,7 @@ fun TopAppBar(
             Text(
                 text = title,
                 style = MpeixTheme.typography.header3,
-                color = MpeixTheme.palette.content,
+                color = LocalContentColor.current,
             )
         },
         modifier = modifier,
@@ -66,6 +68,65 @@ fun TopAppBar(
         actions = actions,
         windowInsets = windowInsets,
         colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MpeixTheme.palette.background,
+            scrolledContainerColor = MpeixTheme.palette.surface,
+            navigationIconContentColor = MpeixTheme.palette.content,
+            titleContentColor = MpeixTheme.palette.content,
+            actionIconContentColor = MpeixTheme.palette.content,
+        ),
+        scrollBehavior = scrollBehavior,
+    )
+}
+
+/**
+ * # LargeTopAppBar
+ *
+ * Mapping implementation to [androidx.compose.material3.LargeTopAppBar].
+ * Top app bars display information and actions at the top of a screen.
+ *
+ * ![Large top app bar image](https://developer.android.com/images/reference/androidx/compose/material3/large-top-app-bar.png)
+ *
+ * This LargeTopAppBar has slots for a title, navigation icon, and actions. In its default expanded
+ * state, the title is displayed in a second row under the navigation and actions.
+ *
+ * @param title the title to be displayed in the top app bar. This title will be used in the app
+ * bar's expanded and collapsed states, although in its collapsed state it will be composed with a
+ * smaller sized [TextStyle]
+ * @param modifier the [Modifier] to be applied to this top app bar
+ * @param navigationIcon the navigation icon displayed at the start of the top app bar. This should
+ * typically be one of:
+ *   - [TopAppBarNavigationIconScope.BackIconButton]
+ *   - [TopAppBarNavigationIconScope.CloseIconButton]
+ * @param actions the actions displayed at the end of the top app bar. This should typically be
+ * [IconButton]s. The default layout here is a [Row], so icons inside will be placed horizontally.
+ * @param windowInsets a window insets that app bar will respect.
+ * @param scrollBehavior a [TopAppBarScrollBehavior] which holds various offset values that will be
+ * applied by this top app bar to set up its height and colors. A scroll behavior is designed to
+ * work in conjunction with a scrolled content to change the top app bar appearance as the content
+ * scrolls. See [TopAppBarScrollBehavior.nestedScrollConnection].
+ */
+@Composable
+fun LargeTopAppBar(
+    title: String,
+    modifier: Modifier = Modifier,
+    navigationIcon: @Composable TopAppBarNavigationIconScope.() -> Unit = {},
+    actions: @Composable RowScope.() -> Unit = {},
+    windowInsets: WindowInsets = TopAppBarDefaults.windowInsets,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+) {
+    androidx.compose.material3.LargeTopAppBar(
+        title = {
+            Text(
+                text = title,
+                style = MpeixTheme.typography.header2,
+                color = LocalContentColor.current,
+            )
+        },
+        modifier = modifier,
+        navigationIcon = { navigationIcon.invoke(TopAppBarNavigationIconScope) },
+        actions = actions,
+        windowInsets = windowInsets,
+        colors = TopAppBarDefaults.largeTopAppBarColors(
             containerColor = MpeixTheme.palette.background,
             scrolledContainerColor = MpeixTheme.palette.surface,
             navigationIconContentColor = MpeixTheme.palette.content,
@@ -114,6 +175,21 @@ fun TopAppBarPreview() {
             TopAppBar(
                 title = "Title",
                 navigationIcon = { CloseIconButton { /* no-op */ } },
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun LargeTopAppBarPreview() {
+    MpeixTheme {
+        Column {
+            LargeTopAppBar(title = "Title")
+            Spacer(modifier = Modifier.height(16.dp))
+            LargeTopAppBar(
+                title = "Title",
+                navigationIcon = { BackIconButton { /* no-op */ } },
             )
         }
     }
