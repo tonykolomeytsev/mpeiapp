c30f1a7d9bcfc27c5dac35f8b1de12ca64fb2b9c
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2023-07-02 23:45:11 +0300

    Fix test configurations for extensions projects

--- build-logic/src/main/kotlin/mpeix/plugins/setup/Constraints.kt
+++ build-logic/src/main/kotlin/mpeix/plugins/setup/Constraints.kt
@@ -8,8 +8,12 @@ internal fun Project.disallowProjectDependencies(reason: () -> String) {
         configurations.all { configuration ->
             configuration.resolutionStrategy.failOnNonReproducibleResolution()
             configuration.resolutionStrategy.eachDependency { details ->
-                if (details.requested.group == rootProject.name) {
-                    error(reason.invoke())
+                // Here details.requested.group always equals root project name if the current
+                // dependency is a subproject. But test tasks require current project itself
+                // as dependency, hence we allow it
+                if (details.requested.group == rootProject.name &&
+                        details.requested.name != name) {
+                    error(details.requested.toString() + "\n" + reason.invoke())
                 }
             }
         }
