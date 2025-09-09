5d702772075e07d5d5d5eb772a7cf0e6c764f28f
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2025-09-09 18:38:24 +0400

    Add Keep annotation to SearchResult

--- modules/feature/schedule/api/src/main/kotlin/kekmech/ru/feature_schedule_api/domain/model/SearchResult.kt
+++ modules/feature/schedule/api/src/main/kotlin/kekmech/ru/feature_schedule_api/domain/model/SearchResult.kt
@@ -1,7 +1,9 @@
 package kekmech.ru.feature_schedule_api.domain.model
 
+import androidx.annotation.Keep
 import java.io.Serializable
 
+@Keep
 public data class SearchResult(
     val id: String,
     val name: String,
