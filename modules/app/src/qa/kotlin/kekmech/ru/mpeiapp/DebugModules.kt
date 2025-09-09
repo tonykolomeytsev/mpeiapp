7ba9c5f727d181f3475e2f8516871a8e93a43d11
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2025-09-09 17:22:13 +0400

    Add qa build variant
    - Added `qa` buildType in `app/build.gradle`.
    - Reorganized and updated launcher icons for debug and qa variants

---
+++ modules/app/src/qa/kotlin/kekmech/ru/mpeiapp/DebugModules.kt
@@ -0,0 +1,12 @@
+package kekmech.ru.mpeiapp
+
+import kekmech.ru.debug_menu.di.DebugMenuModule
+import kekmech.ru.lib_chucker.di.LibraryChuckerModule
+import kekmech.ru.mock_server.di.MockServerModule
+import org.koin.core.module.Module
+
+val DebugModules: List<Module> = listOf(
+    DebugMenuModule,
+    MockServerModule,
+    LibraryChuckerModule,
+)
